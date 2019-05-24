b58fdcf1cd53bd3bc01e6fcf371d7371bab8e6b3
Patricia Hsieh <pathsieh.sd@gmail.com>
2019-05-24 16:53:59 +0000

    edited sizeofvariables.c

--- sizeofvariables.c
+++ sizeofvariables.c
@@ -2,12 +2,20 @@
 
 int main()
 {
-  int a = 545;
+  int a = -545;
   float b = 3.1415;
   char c = 'q';
   double d = 1892.80365;
+  short int e = 2;
+  unsigned int f = 10;
 
   // print size of main types of variables
-  printf("Var a has a size of %d bytes\nVar b has a size of %d bytes\nVar c has a size of %d\nVar d has a size of %d\n", sizeof(a), sizeof(b), sizeof(c), sizeof(d));
+  printf("int %d has a size of %d bytes\n", a, sizeof(a));
+  printf("float %f has a size of %d bytes\n", b, sizeof(b));
+  printf("char %c has a size of %d bytes\n", c, sizeof(c));
+  printf("double %lf has a size of %d bytes\n", d, sizeof(d));
+  printf("short int %hd has a size of %d bytes\n", e, sizeof(e));
+  printf("unsigned int %u has a size of %d bytes\n", f, sizeof(f));
+
 }
 
